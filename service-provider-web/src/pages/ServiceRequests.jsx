4f34a43e39dfaff260d9051be53c81190807feb7
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2021-03-31 00:06:16 +0545

    Implemented data table to display service requests

--- service-provider-web/src/pages/ServiceRequests.jsx
+++ service-provider-web/src/pages/ServiceRequests.jsx
@@ -1,13 +1,64 @@
 import React, { useEffect, useRef, useCallback, useState } from 'react'
 import { useGlobalContext } from '../GlobalContext'
 import axios from 'axios'
+import 'antd/dist/antd.css'
+import { Space, Button } from 'antd'
 import { API_BASE_URI, ROUTING_PATHS, POST_REQUEST_COFIG } from '../constants'
 import AppAlert from '../components/AppAlert'
+import AppDataTable from '../components/AppDataTable'
+
+const SRV_REQUESTS_TABLE_COLS = [
+  {
+    title: 'Client Full Name',
+    dataIndex: 'clientFullName',
+    key: 'clientFullName',
+    // render: text => <a>{text}</a>,
+  },
+  {
+    title: 'Client Id',
+    dataIndex: 'clientId',
+    key: 'clientId',
+  },
+  {
+    title: 'Service Start Date',
+    dataIndex: 'serviceStartDate',
+    key: 'serviceStartDate',
+  },
+  {
+    title: 'Service End Date',
+    dataIndex: 'serviceEndDate',
+    key: 'serviceEndDate',
+  },
+  {
+    title: 'Skill',
+    dataIndex: 'skill',
+    key: 'skill',
+  },
+  // {
+  //   title: 'Service Requirement Desc',
+  //   dataIndex: 'srvRequirementDesc',
+  //   key: 'srvRequirementDesc',
+  // },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+      <Space size='middle'>
+        <Button type='default'>Accept</Button>
+
+        <Button type='default' danger>
+          Reject
+        </Button>
+      </Space>
+    ),
+  },
+]
 
 function ServiceRequests() {
   const { skillRatingsMap, setAppNavigationParams } = useGlobalContext()
   const [errorMsg, setErrorMsg] = useState('')
   const [srvRequestMaps, setSrvRequestMaps] = useState([])
+  const [requestLoading, setRequestLoading] = useState(true)
 
   let sortdSkillRatingsMap = new Map()
   const sortdMapRef = useRef(sortdSkillRatingsMap)
@@ -16,13 +67,19 @@ function ServiceRequests() {
     try {
       const skills = Array.from(sortdMapRef.current.keys())
 
+      setRequestLoading(true)
       const response = await axios.post(
         `${API_BASE_URI}/requests`,
         { selectedSkills: skills },
         POST_REQUEST_COFIG
       )
 
-      setSrvRequestMaps(response.data.data)
+      const responseWithKey = response.data.data.map((d, index) => {
+        const keyObj = { key: index }
+        return { ...d, ...keyObj }
+      })
+      setSrvRequestMaps(responseWithKey)
+      setRequestLoading(false)
     } catch (error) {
       //   console.log(`There was an error: ${error.message}`)
       setErrorMsg(error.message)
@@ -56,12 +113,19 @@ function ServiceRequests() {
   }, [setAppNavigationParams])
 
   return (
-    <>
-      requests... <br />
-      {console.log('imported skills map::' + [...skillRatingsMap.entries()])}
-      <br />
-      {errorMsg ? errorMsg : 'no error to report'}
-    </>
+    <section>
+      <h2>Cheers!! Here is list of matching Service Requests</h2>
+
+      {errorMsg && errorMsg.length !== '' ? (
+        <AppAlert />
+      ) : (
+        <AppDataTable
+          columns={SRV_REQUESTS_TABLE_COLS}
+          data={srvRequestMaps}
+          loading={requestLoading}
+        />
+      )}
+    </section>
   )
 }
 
